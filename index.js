ab52271a99d507018440f9f5b6ffb7792ba5cbf4
Laurent-Philippe Caron <laurentpcaron@gmail.com>
2020-10-30 16:59:05 -0400

    Cycles ok

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const durationInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
+const cycleHeader = document.querySelector('#cycle_name');
 const circle = document.querySelector('circle');
 
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
@@ -9,7 +10,9 @@ circle.setAttribute('stroke-dasharray', perimeter);
 let _duration;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
   onStart(duration) {
-    _duration = duration;
+    if (!_duration) {
+      _duration = duration;
+    }
   },
   onTick(timeLeft) {
     circle.setAttribute(
@@ -17,7 +20,11 @@ const timer = new Timer(durationInput, startBtn, pauseBtn, {
       (perimeter * timeLeft) / _duration - perimeter
     );
   },
-  onComplet() {
-    console.log('Timer completed');
+  onComplet(cycleName) {
+    cycleHeader.innerHTML = cycleName;
+  },
+  onReset() {
+    _duration = null;
+    circle.setAttribute('stroke-dashoffset', 0);
   },
 });
--- timer.js
+++ timer.js
@@ -3,28 +3,50 @@ const DELAY = 20;
 class Timer {
   constructor(durationInput, startBtn, pauseBtn, callbacks) {
     this.durationInput = durationInput;
-    console.log(duration);
     this.startBtn = startBtn;
     this.pauseBtn = pauseBtn;
     if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
       this.onComplet = callbacks.onComplet;
+      this.onReset = callbacks.onReset;
     }
+
+    this.counter = 0;
+    this.cycles = [
+      {
+        name: 'Travail',
+        length: 25,
+        counter: 0,
+      },
+      {
+        name: 'Pause Courte',
+        length: 5,
+        counter: 0,
+      },
+      {
+        name: 'Pause Longue',
+        length: 15,
+      },
+    ];
 
     this.startBtn.addEventListener('click', this.start);
     this.pauseBtn.addEventListener('click', this.pause);
+    this.durationInput.addEventListener('click', this.pause);
+    this.durationInput.addEventListener('change', this.reset);
   }
 
   get timeLeft() {
-    return parseFloat(this.durationInput.value);
+    return parseFloat(this.durationInput.value); ///RENDU À CHANGER LES MILISECONDE EN MINUTE.SECONDE
   }
 
   set timeLeft(time) {
-    this.durationInput.value = time.toFixed(2);
+    this.durationInput.value =
+      Math.floor(time / 60) + ((time % 60) / 100).toFixed(2);
   }
 
   start = () => {
+    this.displayPauseBtn();
     if (this.onStart) {
       this.onStart(this.timeLeft);
     }
@@ -35,21 +57,64 @@ class Timer {
   };
 
   pause = () => {
+    this.displayPlayBtn();
     clearInterval(this.interval);
     this.interval = null;
+  };
+
+  reset = () => {
+    this.pause();
+    if (this.onReset) {
+      this.onReset();
+    }
   };
 
   tick = () => {
     if (this.timeLeft > 0) {
       this.timeLeft -= DELAY / 1000;
-      if (this.onTick) {
-        this.onTick(this.timeLeft);
-      }
     } else {
+      this.timeLeft = 0;
+      this.counter++;
       this.pause();
       if (this.onComplet) {
-        this.onComplet();
+        this.onComplet(this.getCycleName());
       }
+      this.reset();
+    }
+    if (this.onTick) {
+      this.onTick(this.timeLeft);
+    }
+  };
+
+  displayPlayBtn = () => {
+    this.pauseBtn.style.display = 'none';
+    this.startBtn.style.display = 'inline-block';
+  };
+
+  displayPauseBtn = () => {
+    this.pauseBtn.style.display = 'inline-block';
+    this.startBtn.style.display = 'none';
+  };
+
+  getCycleName = () => {
+    switch (this.counter % 6) {
+      case 0:
+      case 2:
+      case 4:
+        this.timeLeft = this.cycles[0].length;
+        return this.cycles[0].name;
+
+      case 1:
+      case 3:
+        this.timeLeft = this.cycles[1].length;
+        return this.cycles[1].name;
+
+      case 5:
+        this.timeLeft = this.cycles[2].length;
+        return this.cycles[2].name;
+
+      default:
+        return 'OMG~BBQ';
     }
   };
 }
